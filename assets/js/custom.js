dfb2a19c25003d9e792330cd3cb698661e0d6cc7
JunIce <east.qrj@gmail.com>
2022-07-07 21:42:58 +0800

    add lunr

---
+++ assets/js/custom.js
@@ -0,0 +1,39 @@
+const lunrSearcher = lunr(function () {
+    this.ref("id")
+    this.field('title');
+    this.field("tags", { boost: 4 });
+    this.field('content');
+
+    fetch("/lunr.json")
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                let item = data[i]
+                item.id = i+1
+                this.add(item)
+            }
+        })
+});
+
+
+window.onload = function () {
+
+    const search = document.querySelector('.search-input')
+
+    console.log(lunrSearcher);
+    
+
+    search.addEventListener('input', (e) => {
+        // console.log(e.target.value);
+
+        let searchStr = e.target.value;
+
+        if (searchStr.trim()) {
+
+            let result = lunrSearcher.search(searchStr.trim());
+
+            console.log('result: ', searchStr.trim(), result);
+        }
+
+    })
+}
