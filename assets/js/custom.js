7fd8aaaee131abb89a722d49c4d63ec4f4d373b7
JunIce <east.qrj@gmail.com>
2022-08-22 21:58:52 +0800

    8/22

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,40 @@
-const lunrSearcher = lunr(function () {
-    this.use(lunr.zh);
-    this.ref("uri")
-    this.field('title');
-    // this.field("tags", { boost: 4 });
-    this.field('content');
+// const lunrSearcher = lunr(function () {
+//     this.use(lunr.zh);
+//     this.ref("uri")
+//     this.field('title');
+//     // this.field("tags", { boost: 4 });
+//     this.field('content');
 
-    fetch("/lunr.json")
-        .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                let item = data[i]
-                // item.id = i+1
-                this.add(item)
-            }
-        })
-});
+//     fetch("/lunr.json")
+//         .then(res => res.json())
+//         .then(data => {
+//             for (let i = 0; i < data.length; i++) {
+//                 let item = data[i]
+//                 // item.id = i+1
+//                 this.add(item)
+//             }
+//         })
+// });
 
 
-window.onload = function () {
+// window.onload = function () {
 
-    const search = document.querySelector('.search-input')
+//     const search = document.querySelector('.search-input')
 
-    console.log(lunrSearcher);
+//     console.log(lunrSearcher);
     
 
-    search.addEventListener('input', (e) => {
-        // console.log(e.target.value);
+//     search.addEventListener('input', (e) => {
+//         // console.log(e.target.value);
 
-        let searchStr = e.target.value;
+//         let searchStr = e.target.value;
 
-        if (searchStr.trim()) {
+//         if (searchStr.trim()) {
 
-            let result = lunrSearcher.search(searchStr.trim());
+//             let result = lunrSearcher.search(searchStr.trim());
 
-            console.log('result: ', searchStr.trim(), result);
-        }
+//             console.log('result: ', searchStr.trim(), result);
+//         }
 
-    })
-}
+//     })
+// }
