a7d6fd50049152d93c0241e2428a8c23adbda416
JunIce <east.qrj@gmail.com>
2023-04-18 21:34:42 +0800

    search pattern

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,3 +58,53 @@ function addCopyButton() {
     document.body.appendChild(script);
   }
 }
+
+
+var index = new FlexSearch.Document({
+  tokenize: "forward",
+  cache: 100,
+  document: {
+    id: 'id',
+    store: [
+      "href", "title", "description"
+    ],
+    index: ["title", "description", "content"]
+  },
+  encode: str => str.split("")
+});
+
+{{ $list := .Site.Pages }}
+{{ range $index, $element := $list -}}
+  index.add(
+    {
+      id: {{ $index }},
+      href: "{{ .RelPermalink }}",
+      title: {{ .Title | jsonify }},
+      content: {{ .Plain | jsonify }}
+    }
+  );
+{{ end -}}
+
+
+
+const search = document.querySelector("#search-container")
+
+search.addEventListener("input", doSearchResult, true)
+
+
+
+function doSearchResult() {
+  const searchQuery = this.value;
+
+  const searchResult = index.search(searchQuery, { limit: 5, enrich: true })
+
+
+  const flatResults = new Map(); // keyed by href to dedupe results
+  for (const result of searchResult.flatMap(r => r.result)) {
+    if (flatResults.has(result.doc.href)) continue;
+    flatResults.set(result.doc.href, result.doc);
+  }
+
+
+  console.log(flatResults)
+}
